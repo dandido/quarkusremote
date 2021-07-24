86402f71d976c87c9cd45df087c8a00a7c8febf6
Hajjar Med Chedly <chhajjar2006@gmail.com>
2021-07-24 23:01:42 +0100

    Adding tempales

--- src/main/java/org/chedly/GreetingResource.java
+++ src/main/java/org/chedly/GreetingResource.java
@@ -1,16 +1,46 @@
 package org.chedly;
 
+import java.util.Arrays;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    // le meme nom que le fichier
+    Template helloW;
+
+    @Inject
+    Template myhtml;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from remote Boom";
+    }
+
+    @GET
+    @Path("/gettemp")
+    @Produces(MediaType.TEXT_PLAIN)
+    public TemplateInstance mytemp() {
+        return helloW.data("name","Chedly?");
+    }
+
+    @GET
+    @Path("/gethtml")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance mygethtml() {
+        TemplateInstance  i = myhtml
+        .data("name","Chedly via template?")
+        .data("mylist",Arrays.asList("i1","i2","i3"));
+        return i;
     }
 }
